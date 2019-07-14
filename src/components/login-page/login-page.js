0f693ffec6814773dec64e4f22615e8e47a898a7
Kirill Kagdin <rlyiamberserker@gmail.com>
2019-07-14 18:09:09 +0300

    ++validation for LoginForm

--- src/components/login-page/login-page.js
+++ src/components/login-page/login-page.js
@@ -6,22 +6,34 @@ import {loginUser} from "../../redux/store/auth-reducer";
 import {Redirect} from "react-router-dom";
 import style from "./login-page.module.css"
 
+const email = value =>
+    value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ?
+        'Некорректный email' : undefined;
+const required = value => value ? undefined : 'Обязательное поле';
+
+
+const renderField = ({input, label, type, meta: {touched, error, warning}}) => {
+    return <div>
+        <div className={"form-group"}>
+            <label>{label}</label>
+            <input className={(touched && ((error && "form-control is-invalid")))
+            || (touched && (!error && "form-control is-valid"))
+            || "form-control"} {...input} placeholder={label} type={type}/>
+            {touched && ((error && <span style={{color: 'red', fontWeight: 'bold'}}>{error}</span>)
+                || (warning && <span style={{color: 'red', fontWeight: 'bold'}}>{warning}</span>))}
+        </div>
+    </div>
+};
 
 const LoginForm = (props) => {
     return <div className={style.login__form}>
         <div className='row'>
             <div className='col-sm-8 offset-2'>
                 <form onSubmit={props.handleSubmit}>
-                    <div className={'form-group'}>
-                        <label htmlFor="InputEmail1">Email address</label>
-                        <Field name={'login'} id={'InputEmail1'} component={'input'} type={'text'}
+                        <Field validate={[required, email]} label={"Email"} name={'login'} id={'InputEmail1'} component={renderField} type={'text'}
                                className="form-control"/>
-                    </div>
-                    <div className={'form-group'}>
-                        <label htmlFor="InputPassword1">Password</label>
-                        <Field name={'password'} id="InputPassword1" component={'input'} type={'password'}
+                        <Field validate={required} label={"Password"} name={'password'} id="InputPassword1" component={renderField} type={'password'}
                                className="form-control"/>
-                    </div>
                     <div className={'form-check'}>
                         <Field name={'rememberMe'} id="Check1" component={'input'} type={'checkbox'}
                                className="form-check-input"/>
