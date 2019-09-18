f8def3624ff0ab95aa59b028f0037a6358c23948
Kirill Kagdin <rlyiamberserker@gmail.com>
2019-09-18 14:35:48 +0300

    Updates

---
+++ precache-manifest.628151f79fcace9299e08f6392c54d89.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76bee0d53b9842c726c6dc732c64cbe0",
+    "url": "/looking-it-react-app/index.html"
+  },
+  {
+    "revision": "eef27f55dd3af2ab2397",
+    "url": "/looking-it-react-app/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "c6ee4955b49ee98eb59b",
+    "url": "/looking-it-react-app/static/css/main.e2c11a9f.chunk.css"
+  },
+  {
+    "revision": "eef27f55dd3af2ab2397",
+    "url": "/looking-it-react-app/static/js/2.470f4221.chunk.js"
+  },
+  {
+    "revision": "c6ee4955b49ee98eb59b",
+    "url": "/looking-it-react-app/static/js/main.3af6beef.chunk.js"
+  },
+  {
+    "revision": "7faf6c93cadbf40af069",
+    "url": "/looking-it-react-app/static/js/runtime~main.e467f560.js"
+  },
+  {
+    "revision": "8f765de390556dd659dfc77ec1ad9cc6",
+    "url": "/looking-it-react-app/static/media/Preloader_7.8f765de3.gif"
+  },
+  {
+    "revision": "347f5bab042f9682bb67886cc613e482",
+    "url": "/looking-it-react-app/static/media/comment_icon.347f5bab.png"
+  },
+  {
+    "revision": "02afb8176c7f9ea92e61439495cd6ccd",
+    "url": "/looking-it-react-app/static/media/dialogue.02afb817.png"
+  },
+  {
+    "revision": "b2a26cf9adf80cdde16da6cd634ca4a7",
+    "url": "/looking-it-react-app/static/media/like_icon.b2a26cf9.png"
+  },
+  {
+    "revision": "29e517d233dd119fbfeca2949adbf5f3",
+    "url": "/looking-it-react-app/static/media/logo.29e517d2.png"
+  },
+  {
+    "revision": "12d5444354ea463464a934cf66246d68",
+    "url": "/looking-it-react-app/static/media/news.12d54443.png"
+  },
+  {
+    "revision": "1527b0641404672b4ad5bfa9dffc557f",
+    "url": "/looking-it-react-app/static/media/noavatar.1527b064.png"
+  },
+  {
+    "revision": "6f1fb8cd4e0e729ece2bf99714dc6bcd",
+    "url": "/looking-it-react-app/static/media/preloader.6f1fb8cd.gif"
+  },
+  {
+    "revision": "245e9a5d6cf83ab769d00f4f80dd4085",
+    "url": "/looking-it-react-app/static/media/profile.245e9a5d.png"
+  },
+  {
+    "revision": "0e319f53c87ee8a4d4337266422a22e8",
+    "url": "/looking-it-react-app/static/media/repost_icon.0e319f53.png"
+  },
+  {
+    "revision": "1fffe1a0f89a3becebd13cc219a9afa9",
+    "url": "/looking-it-react-app/static/media/settings.1fffe1a0.png"
+  },
+  {
+    "revision": "b4656379b41c2338453bee7fc35dc063",
+    "url": "/looking-it-react-app/static/media/user_male.b4656379.png"
+  },
+  {
+    "revision": "fefb1b275edefcb0e469eb7b25977595",
+    "url": "/looking-it-react-app/static/media/users.fefb1b27.png"
+  }
+]);
